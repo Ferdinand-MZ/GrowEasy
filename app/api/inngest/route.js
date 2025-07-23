465d4c7800a15d4ffaa2ef398674122e2111ed28
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2025-07-23 23:50:57 +0700

    bruh

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,6 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-<<<<<<< HEAD
-=======
-
->>>>>>> c408a44 (First Commit)
 import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
